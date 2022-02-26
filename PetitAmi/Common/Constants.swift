68f1b3c75b96074fa33466f4567f0bb31a35bcbb
marcelosimim <54868944+marcelosimim@users.noreply.github.com>
2022-02-25 23:28:31 -0300

    launch screen, colors, images and folders

---
+++ PetitAmi/Common/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  PetitAmi
+//
+//  Created by Marcelo Simim on 25/02/22.
+//
+
+import Foundation
+import UIKit
+
+class K {
+    
+    //MARK: - Colors
+    static let primaryColor = UIColor(named: "Primary")
+    static let buttonColor = UIColor(named: "Button")
+    
+    //MARK: - Images
+    static let logoImage = UIImage(named: "Logo")
+}
